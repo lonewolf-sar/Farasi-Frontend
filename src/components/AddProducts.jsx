30451f43a7916ae3a021d167166dd8ae206beb6d
lonewolf-sar <karbolojoshua@gmail.com>
2026-03-17 14:32:54 +0300

    update addproducts.jsx

--- src/components/AddProducts.jsx
+++ src/components/AddProducts.jsx
@@ -1,9 +1,81 @@
 import React from 'react'
-
+import{Link, useNavigate} from 'react-router-dom'
+import{ useState } from 'react'
+import axios from "axios"
 const AddProducts = () => {
+  const[product_name,setProduct_name]=useState();
+  const[product_description,setProduct_description]=useState();
+  const[product_cost,setProduct_cost]=useState();
+  const[product_photo,setProduct_photo]=useState();
+ // initialize other hooks for getting loading, success,error
+
+  const[loading,setLoading]=useState();
+  const[success,setSuccess]=useState();
+  const[error,setError]=useState();
+
+
+
+  const submit=async(e)=>{
+    e.preventDefault()
+    setLoading("product added successfully")
+
+    try {
+      const data=new FormData();
+      data.append("product_name",product_name);
+      data.append("product_description",product_description);
+      data.append("product_cost",product_cost);
+      data.append("product_photo",product_photo);
+
+      const response=await axios.post("http://joshuakarbolo.alwaysdata.net/api/addproducts",data)
+
+      setLoading("")
+      setSuccess(response.data.message)
+      setProduct_name("")
+      setProduct_description("")
+      setProduct_cost("")
+      setProduct_photo("")
+    } catch (error) {
+      setLoading("")
+      setError(error.message)
+      
+    }
+  }
+
+
   return (
-    <div>
-        <h1>Welcome To add Products</h1>
+    <div className='row justify-content-center m-top-3'>
+      <div className='card shadow col-md-6'> 
+        
+        <form action="" onSubmit={submit}>
+          <h1>Upload your Products</h1>
+          <p className='text-warening'>{loading}</p>
+          <p className='text-success'>{success}</p>
+          <p className='text-danger'>{error}</p>
+
+          <input type="text" placeholder='Enter Product Name'className='form-control'required value={product_name}onChange={(e)=>setProduct_name(e.target.value)}/>
+  
+
+          <input type="text" placeholder='Describe your Product'className='form-control'require value={product_description} onChange={(e)=>setProduct_description(e.target.value)}/>
+          <input type="number" placeholder='Enter Product Cost' className='form-control'required value={product_cost} onChange={(e)=>setProduct_cost(e.target.value)}/>
+
+          
+
+          <h2>Upload Product Photo</h2>
+
+          <input type="file"className='form-control' accept="image/*"onChange={(e)=>setProduct_photo(e.target.files[0])} required/>
+          
+          <input type="submit"value='Upload Product'className='form-control bg-success text-white w-100'/>
+
+
+
+        
+
+
+
+        </form>
+
+      </div>
+        
     </div>
   )
 }
--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,93 @@
 import React from 'react'
+import{Link, useNavigate} from 'react-router-dom'
+import{ useState } from 'react'
+import axios from "axios"
 
 const SignIn = () => {
+
+  const[email,setEmail]=useState("");
+  const[password,setPassword]=useState("");
+
+  // initialize our hooks
+
+  const[loading,setLoading]=useState("");
+  const[success,setSuccess]=useState("");
+  const[error,setError]=useState("");
+
+// a hook used to redirect a user
+  const navigate=useNavigate();
+
+  // function to send data to our database
+  const submit=async(e)=>{
+    e.preventDefault()
+    setLoading("Please wait as we upload your data")
+    // send data to our database
+    try {
+      const data=new FormData
+      data.append("email",email)
+      data.append("password",password)
+      // post above data to backend API
+      // calling our API
+
+      const response=await axios.post("http://joshuakarbolo.alwaysdata.net/api/signin",data)
+
+      setLoading("")
+      // check if response has user item
+      if (response.data.user){
+        // if user is found,store user details in localstorage
+        localStorage.setItem("user",JSON.stringify(response.data.user));
+        setSuccess(response.data.message);
+
+        // redirect to /getproducts component
+        setTimeout(()=>{
+          navigate("/");
+        },2000)
+      }
+      else{
+        // user not found ,show error message
+        setError(response.data.message);
+      }
+    
+
+      
+    } catch (error) {
+      setLoading("")
+      setError(error.data.message)
+
+      
+    }
+
+  }
+
   return (
-    <div>
-        <h1>Welcome To SignIn</h1>
+    <div className='row justify-content-center m-top-3'>
+      <div className='card shadow col-md-6'>
+        <h1>SignIn</h1>
+        <form action="" onSubmit={submit}>
+         <p className='text-warning'>{loading}</p>
+         <p className='text-success'>{success}</p>
+         <p className='text-danger'>{error}</p>
+
+         <input type="text"placeholder='Email'className='form-control' required value={email} onChange={(e)=>setEmail (e.target.value)}/>
+         {email}
+
+         <input type="text" placeholder='Password' className='form-control'required value={password} onChange={(e)=>setPassword (e.target.value)}/>
+         {password}
+
+         <input type="submit"value='SignIn'className='w-100 form-control text-white bg-success'required/>
+         
+         <p>Don't have an account?<Link to='signup'>SignUp</Link></p>
+
+
+          
+
+
+
+
+        </form>
+
+      </div>
+      
     </div>
   )
 }
